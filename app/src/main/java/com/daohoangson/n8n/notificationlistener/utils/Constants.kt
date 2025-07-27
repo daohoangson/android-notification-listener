4c7e422dbc3e45141facb01f6a3a0aa59f7b7873
Dao Hoang Son <daohoangson@gmail.com>
2025-07-27 21:50:34 +0700

    Human: remove build configs

--- app/src/main/java/com/daohoangson/n8n/notificationlistener/utils/Constants.kt
+++ app/src/main/java/com/daohoangson/n8n/notificationlistener/utils/Constants.kt
@@ -3,8 +3,6 @@ package com.daohoangson.n8n.notificationlistener.utils
 import com.daohoangson.n8n.notificationlistener.BuildConfig
 
 object Constants {
-    val WEBHOOK_BASE_URL: String = BuildConfig.WEBHOOK_BASE_URL
-    val WEBHOOK_PATH: String = BuildConfig.WEBHOOK_PATH
     const val DATABASE_NAME = "notification_listener_db"
     const val DATABASE_VERSION = 2
     const val NETWORK_TIMEOUT_SECONDS = 30L
